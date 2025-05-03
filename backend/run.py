00f83cdae10e013eb87d8a72e13f4de947b1758d
Sharvari More <146668445+Sharvari-21@users.noreply.github.com>
2025-05-03 22:11:20 +0530

    Update run.py

--- backend/run.py
+++ backend/run.py
@@ -1,3 +1,4 @@
+import os
 from app import create_app
 
 app = create_app()
@@ -6,8 +7,12 @@ app = create_app()
 def home():
     return "Backend is running!"
 
-app.config['ENV'] = 'development'
-app.config['DEBUG'] = True
+# Set app configurations
+app.config['ENV'] = 'production'
+app.config['DEBUG'] = False
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Get the port from the environment, default to 5000
+    port = int(os.environ.get("PORT", 5000))
+    # Run the app on 0.0.0.0 to make it externally accessible
+    app.run(host='0.0.0.0', port=port)
